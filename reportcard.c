10c51e58c290d2a04d5f4a5bd57d8dba61b560cc
Brigitta-28 <68139998+Brigitta-28@users.noreply.github.com>
2020-07-11 12:08:54 +0530

    Update reportcard.c

--- reportcard.c
+++ reportcard.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int mark=0;
 printf("Enter the mark of student:");
 scanf("%d",&mark);
 
-if(mark>=85&&<=100)
+if(mark>=85 && mark<=100)
 printf("Grade A");
-else if(mark>=70&&<=84)
+else if(mark>=70 && mark<=84)
 printf("Grade B");
-else if(mark>=55&&<=69)
+else if(mark>=55 && mark<=69)
 printf("Grade C");
-else if(mark>=40&&<=54)
+else if(mark>=40 && mark<=54)
 printf("Grade D");
 else
 printf("Grade F");
